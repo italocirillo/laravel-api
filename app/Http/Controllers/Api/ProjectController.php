67896274cb20bf776f7d0d8d75bd3a3a69fe54c0
italocirillo <italocirillo@gmail.com>
2023-06-15 00:10:29 +0200

    ESERCIZIO COMPLETO + TUTTI I BONUS

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with(['type', 'technology'])->paginate(20);
+        $projects = Project::with(['type', 'technologies'])->paginate(20);
         return response()->json([
             'success' => true,
             'results' => $projects
