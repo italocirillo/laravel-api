495d82f1d68af462a271193b8c1271b236e2d5f4
italocirillo <italocirillo@gmail.com>
2023-06-17 18:37:45 +0200

    ESERCIZIO COMPLETO + BONUS

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('projects/{slug}', [ProjectController::class, 'show']);
 Route::get('types', [TypeController::class, 'index']);
